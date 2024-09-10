56a7b0261c574b7eba71d2f135e34d573cbc70f6
siam3h <philosiama@gmail.com>
2024-09-10 14:08:13 +0300

    Update tests

--- tests/test_payment.py
+++ tests/test_payment.py
@@ -1,21 +1,39 @@
+from unittest.mock import patch
 import unittest
 from modules.processor import PaymentProcessor
 
 class TestPaymentProcessor(unittest.TestCase):
-    def setUp(self):
-        self.payment_processor = PaymentProcessor()
 
-    def test_initialize_payment(self):
-        email = "test@example.com"
-        amount = 5000
-        callback_url = "http://your-site.com/callback"
-        response = self.payment_processor.initialize_payment(email, amount, callback_url)
+    @patch('modules.client.PaystackClient.post')
+    def test_initialize_payment(self, mock_post):
+        # Mock Paystack API response
+        mock_post.return_value = {
+            'status': True,
+            'data': {
+                'authorization_url': 'https://paystack.com/authorize'
+            }
+        }
+
+        payment_processor = PaymentProcessor()
+        response = payment_processor.initialize_payment('test@example.com', 5000, 'https://example.com/callback')
+
         self.assertIn('status', response)
         self.assertTrue(response['status'])
 
-    def test_verify_payment(self):
-        reference = "dummy_reference"
-        response = self.payment_processor.verify_payment(reference)
+    @patch('modules.client.PaystackClient.get')
+    def test_verify_payment(self, mock_get):
+        # Mock  Paystack API response
+        mock_get.return_value = {
+            'status': True,
+            'data': {
+                'reference': 'nmug42lyfz',
+                'status': 'success'
+            }
+        }
+
+        payment_processor = PaymentProcessor()
+        response = payment_processor.verify_payment('nmug42lyfz')
+
         self.assertIn('status', response)
         self.assertTrue(response['status'])
 
